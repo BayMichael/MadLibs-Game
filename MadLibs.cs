c78b24a72c3e0fa3dd436573938049b21f1b9ae1
Michael Bayer <yoyomike13@gmail.com>
2023-05-16 16:27:13 +0200

    First Commit

---
+++ MadLibs.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MadLibs
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      /*
+      This program will run an MadLibs word game 
+      Author: Michael Bayer
+      */
+        Console.WriteLine("Hello World");
+    }
+  }
+}
